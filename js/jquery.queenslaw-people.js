48d14b8b8d834cdf171ec9bb695163c16bc6ab28
noah-st-amand <noah@tookish.net>
2018-09-17 19:03:34 -0400

    Revised javascript to check some types automatically

--- js/jquery.queenslaw-people.js
+++ js/jquery.queenslaw-people.js
@@ -1,25 +1,32 @@
 (function ($) {
   $(document).ready(function(){
     $('body.landing-page-directory .view-people, #block-views-block-people-faculty-block .view-people').wrap('<div class="people-dynamic"/>');
-    initializePeopleDynamic();
+    var filters_on = [];
+    if ($('#block-views-block-people-faculty-block').length > 0) filters_on = [1, 2];
+    initializePeopleDynamic(filters_on);
   });
   $(document).ajaxComplete(function(){
-    initializePeopleDynamic();
+    initializePeopleDynamic(false);
   });
-  function initializePeopleDynamic() {
-    updatePeople(true);
+  function initializePeopleDynamic(filters_on) {
+    updatePeople(true, filters_on);
     $('.people-dynamic input.form-checkbox').change(function(){
-      updatePeople(false);
+      updatePeople(false, false);
     });
     $('.people-dynamic .clear-all-filters').click(function(event){
       clearFilters();
       event.preventDefault();
     });
   }
-  function updatePeople(add_elements) {
+  function updatePeople(add_elements, filters_on) {
     if (add_elements) {
       $('.people-dynamic .form-item-filter').after('<a class="clear-all-filters" href="#">Clear all filters</a>');
       $('.people-dynamic .view-body').append('<div class="empty people-dynamic-empty">No people were found matching the criteria provided. Please try again.</div>');
+      if (filters_on) {
+        for (i = 0; i < filters_on.length; i++) {
+          $('.people-dynamic .form-checkboxes .form-item input#edit-person-type-' + filters_on[i]).prop('checked', true);
+        }
+      }
     }
     $('.people-dynamic').removeClass('empty');
     $('.people-dynamic .views-row').removeClass('views-row-even');
